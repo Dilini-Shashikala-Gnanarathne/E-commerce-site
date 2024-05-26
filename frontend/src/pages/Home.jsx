e3e8d0f6ca507b47d57f0c28d5902d101921490d
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-05-26 14:51:03 +0530

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 const Home = () => {
+const[number, setNumber]=useState(null);
+const[startdate,setStartdate]=useState(null);
+
+
+
   return <>
+
+
+          <div className='bg-cellwhiteColor w-full max-w-[80%] mx-auto rounded-lg shadow-md md:p-10 object-cover'>
+          <div className="mb-5">
+            <input type='number' placeholder='Enter Your Password' name='number' 
+            className=' text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required
+            />
+          </div>
+          <div className="mb-5">   
+            <input type='date' placeholder='Enter Your Email' name='startdate'
+            className=' text-headingColor placeholder:text-textColor  cursor-pointer' required
+            />
+          </div>
+          <div className="mb-5">
+            <input type='date' placeholder='Enter Your Password' name='enddate' 
+            className=' text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required
+            />
+          </div>
+         
+          </div>
    
-   Dashbord
   </>
 }
 
