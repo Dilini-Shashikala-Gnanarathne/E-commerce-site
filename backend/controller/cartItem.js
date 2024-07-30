634622d1aeb129407ce36f70f645e8e0d47c6653
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-07-30 15:58:12 +0530

    add cart item details

---
+++ backend/controller/cartItem.js
@@ -0,0 +1,53 @@
+const User = require('../models/UserSchema');
+
+const updateCart = async (req, res, cartField) => {
+  const { email, totalAmount } = req.body;
+
+  try {
+    console.log('Request Body:', req.body);
+
+    if (typeof totalAmount !== 'number') {
+      return res.status(400).json({
+        success: false,
+        message: 'Total amount must be a number',
+      });
+    }
+
+    let record = await User.findOne({ email });
+    console.log('Record:', record);
+
+    if (record) {
+      if (!record[cartField]) {
+        record[cartField] = { totalAmount: 0 };
+      }
+
+      record[cartField].totalAmount = totalAmount;
+
+      await record.save();
+
+      return res.status(200).json({
+        success: true,
+        message: 'Successfully updated',
+        data: record,
+      });
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'Record not found',
+      });
+    }
+  } catch (err) {
+    console.error('Error:', err);
+    return res.status(500).json({
+      success: false,
+      message: 'Internal server error',
+      err: err.message,
+    });
+  }
+};
+
+const cart = (req, res) => updateCart(req, res, 'cart');
+
+module.exports = {
+  cart
+};
--- backend/router.js
+++ backend/router.js
@@ -3,11 +3,11 @@ const { authenticateToken, verifyUser } = require('./auth/verifyToken');
 const routers = require('./controller/auth');
 const router = express.Router();
 const getUser= require('./controller/getUser');
-
+const item=require('./controller/cartItem')
 router.post('/createUser', routers.register);
 router.post('/login', routers.login);
 router.get('/getUser', getUser.getUserFunction);
-
+router.post('/cart', item.cart)
 
 router.get('/user',authenticateToken, verifyUser);
 
